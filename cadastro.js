0ecb960b1bedc84ee3261ea0ae311d3ba91adebd
yagoal <br.yago@gmail.com>
2022-05-17 16:41:40 -0300

    path img and register

---
+++ cadastro.js
@@ -0,0 +1,130 @@
+
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+           
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {    
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+        
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    function isCPF (c) {
+
+        if((c = c.replace(/[^\d]/g,"")).length != 11)
+        return false
+
+        if (c == "00000000000")
+        return false;
+
+        var r;
+        var s = 0;
+
+        for (i=1; i<=9; i++)
+            s = s + parseInt(c[i-1]) * (11 - i);
+
+            r = (s * 10) % 11;
+
+            if ((r == 10) || (r == 11))
+            r = 0;
+
+        if (r != parseInt(c[9]))
+            return false;
+
+        s = 0;
+
+        for (i = 1; i <= 10; i++)
+            s = s + parseInt(c[i-1]) * (12 - i);
+
+        r = (s * 10) % 11;
+
+        if ((r == 10) || (r == 11))
+        r = 0;
+
+        if (r != parseInt(c[10]))
+            return false;
+
+        return true;
+        }
+
+
+        function fMasc(objeto,mascara) {
+        obj=objeto
+        masc=mascara
+        setTimeout("fMascEx()",1)
+        }
+
+        function fMascEx() {
+        obj.value=masc(obj.value)
+        }
+
+        function mCPF(cpf){
+        cpf=cpf.replace(/\D/g,"")
+        cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+        cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+        cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+
+    return cpf
+}
+
+cpfCheck = function (el) {
+    let string = isCPF(el.value)? '<span style="color:green">CPF</span>' : '<span style="color:red">CPF Inválido</span>';
+    if(el.value.length == 14) {
+        document.getElementById('cpfResponse').innerHTML = document.getElementById('cpfResponse').innerHTML = string
+    } 
+}
